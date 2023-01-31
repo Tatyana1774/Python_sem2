b49d8668f3f16cd5c758f0137b13a0f28b607b3e
Tatyana <tatyana.volkova1984@mail.ru>
2023-01-31 23:25:57 +0500

    Задачи 2 семинар

---
+++ 1.py
@@ -0,0 +1,8 @@
+for i in range(3, 10):
+    print(i)
+
+
+# range(n) - 0, 1, 2..., n - 1
+# range(3, n) - 3, 4, ..., n - 1
+# range(10, 0, -1) - 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+# range(begin, end, step)
